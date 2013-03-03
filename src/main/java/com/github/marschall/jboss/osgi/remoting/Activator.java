232ae2f18183d76ed8b352b6b2a91db45f21fc5f
Philippe Marschall <philippe.marschall@gmail.com>
2013-03-03 07:54:32 +0100

    sketch more

--- src/main/java/com/github/marschall/jboss/osgi/remoting/Activator.java
+++ src/main/java/com/github/marschall/jboss/osgi/remoting/Activator.java
@@ -3,8 +3,6 @@ package com.github.marschall.jboss.osgi.remoting;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleEvent;
-import org.osgi.framework.BundleListener;
 
 public class Activator implements BundleActivator {
   
@@ -12,7 +10,7 @@ public class Activator implements BundleActivator {
 
   @Override
   public void start(BundleContext context) throws Exception {
-    this.proxyService = new ProxyService();
+    this.proxyService = new ProxyService(context);
     context.removeBundleListener(this.proxyService);
     
     Bundle[] bundles = context.getBundles();
--- src/main/java/com/github/marschall/jboss/osgi/remoting/ProxyService.java
+++ src/main/java/com/github/marschall/jboss/osgi/remoting/ProxyService.java
@@ -9,7 +9,12 @@ import static javax.xml.stream.XMLInputFactory.SUPPORT_DTD;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -17,18 +22,23 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
 
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
+import org.osgi.framework.ServiceRegistration;
 
 final class ProxyService implements BundleListener {
   
   private final ConcurrentMap<Bundle, BundleProxyContext> contexts;
   
   private final XMLInputFactory inputFactory;
+
+  private final BundleContext bundleContext;
   
 
-  ProxyService() {
-    this.contexts = new ConcurrentHashMap<>();
+  ProxyService(BundleContext bundleContext) {
+    this.bundleContext = bundleContext;
+    this.contexts = new ConcurrentHashMap<Bundle, BundleProxyContext>();
     this.inputFactory = this.createInputFactory();
   }
 
@@ -56,29 +66,57 @@ final class ProxyService implements BundleListener {
     // TODO check
     URL serviceXml = bundle.getResource("OSGI-INF/services/service.xml");
     if (serviceXml != null) {
-      this.parseServiceXml(serviceXml);
+      ParseResult result = this.parseServiceXml(serviceXml);
+      
+      if (!result.isEmpty()) {
+        // TODO parent = this parent?
+        ClassLoader classLoader = new BundleProxyClassLoader(bundle);
+        Collection<ServiceCaller> callers = new ArrayList<ServiceCaller>(result.size());
+        Collection<ServiceRegistration<?>> registrations = new ArrayList<ServiceRegistration<?>>(result.size());
+        for (ServiceInfo info : result.services) {
+          Class<?> interfaceClazz = classLoader.loadClass(info.interfaceName);
+          Object jBossProxy = this.lookUpJBossProxy(interfaceClazz, info.jndiName);
+          ServiceCaller serviceCaller = new ServiceCaller(jBossProxy, classLoader);
+          callers.add(serviceCaller);
+          // TODO properties
+          Dictionary<String, Object> properties = new Hashtable<String, Object>();
+          ServiceRegistration<?> serviceRegistration = this.bundleContext.registerService(info.interfaceName, serviceCaller, properties);
+          registrations.add(serviceRegistration);
+        }
+        BundleProxyContext bundleProxyContext = new BundleProxyContext(callers, registrations);
+        this.contexts.put(bundle, bundleProxyContext);
+      }
     }
   }
   
-  private void parseServiceXml(URL serviceXml) {
-    try (InputStream stream = serviceXml.openStream()) {
+  private Object lookUpJBossProxy(Class<?> interfaceClazz, String jndiName) {
+    Object proxy = null;
+    return interfaceClazz.cast(proxy);
+  }
+  
+  private ParseResult parseServiceXml(URL serviceXml) {
+    InputStream stream = serviceXml.openStream();
+    try {
       XMLStreamReader reader = this.inputFactory.createXMLStreamReader(stream);
       try {
-        
+
       } finally {
         // TODO CR
         reader.close();
       }
     } catch (IOException e) {
-      
+
+    } finally {
+      stream.close();
     }
-    
+
   }
 
   void removePotentialBundle(Bundle bundle) {
     BundleProxyContext context = this.contexts.remove(bundle);
     if (context != null) {
-      context.release();
+      context.invalidateCallers();
+      context.unregisterServices(bundleContext);
     }
   }
   
@@ -98,15 +136,54 @@ final class ProxyService implements BundleListener {
     
   }
   
+  static final class ParseResult {
+    
+    final List<ServiceInfo> services;
+
+    ParseResult(List<ServiceInfo> services) {
+      this.services = services;
+    }
+    
+    boolean isEmpty() {
+      return this.services.isEmpty();
+    }
+    
+    int size() {
+      return this.services.size();
+    }
+    
+  }
+  
+  static final class ServiceInfo {
+    
+    final String interfaceName;
+    final String jndiName;
+    
+    ServiceInfo(String interfaceName, String jndiName) {
+      this.interfaceName = interfaceName;
+      this.jndiName = jndiName;
+    }
+    
+  }
+  
   static final class BundleProxyContext {
     
     private final Collection<ServiceCaller> callers;
+    
+    private final Collection<ServiceRegistration<?>> registrations;
 
-    BundleProxyContext(Collection<ServiceCaller> callers) {
+    BundleProxyContext(Collection<ServiceCaller> callers, Collection<ServiceRegistration<?>> registrations) {
       this.callers = callers;
+      this.registrations = registrations;
     }
     
-    void release() {
+    void unregisterServices(BundleContext bundleContext) {
+      for (ServiceRegistration<?> registration : this.registrations) {
+        bundleContext.ungetService(registration.getReference());
+      }
+    }
+    
+    void invalidateCallers() {
       for (ServiceCaller caller : callers) {
         caller.invalidate();
       }
