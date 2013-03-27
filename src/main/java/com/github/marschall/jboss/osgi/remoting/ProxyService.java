1568d567c7a49f7d0053b08e5a283d80ca55d88e
Philippe Marschall <philippe.marschall@gmail.com>
2013-03-27 20:46:13 +0100

    work on processor

--- src/main/java/com/github/marschall/jboss/osgi/remoting/ProxyService.java
+++ src/main/java/com/github/marschall/jboss/osgi/remoting/ProxyService.java
@@ -50,6 +50,7 @@ final class ProxyService implements BundleListener {
   // mvn dependency:copy-dependencies -DoutputDirectory=lib
   // unzip -c jboss-transaction-api_1.1_spec-1.0.1.Final.jar META-INF/MANIFEST.MF
   private static final String[] PARENT_BUNDLE_IDS = {
+    // TODO jboss specific
     "org.jboss.spec.javax.transaction.jboss-transaction-api_1.1_spec",
     "org.jboss.spec.javax.ejb.jboss-ejb-api_3.1_spec",
     // missing SASL bundle
--- src/main/java/com/github/marschall/jboss/osgi/remoting/ServiceXmlGenerator.java
+++ src/main/java/com/github/marschall/jboss/osgi/remoting/ServiceXmlGenerator.java
@@ -2,9 +2,11 @@ package com.github.marschall.jboss.osgi.remoting;
 
 import static javax.lang.model.SourceVersion.RELEASE_6;
 
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedOptions;
@@ -24,11 +26,28 @@ import javax.lang.model.element.TypeElement;
 })
 public class ServiceXmlGenerator extends AbstractProcessor {
   
+  public ServiceXmlGenerator() {
+    super();
+  }
+  
+  @Override
+  public void init(ProcessingEnvironment processingEnv) {
+    super.init(processingEnv);
+
+    Map<String, String> options = processingEnv.getOptions();
+  }
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-    // TODO Auto-generated method stub
-    return false;
+    if (roundEnv.errorRaised()) {
+      return false;
+    } else if (roundEnv.processingOver()) {
+      // write xml
+      return false;
+    } else {
+      // collect data
+      return true;
+    }
   }
 
 }
