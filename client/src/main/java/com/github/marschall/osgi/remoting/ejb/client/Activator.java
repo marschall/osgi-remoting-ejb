0b432e1e08fc5691fddb0c70114bd845829ca621
Philippe Marschall <philippe.marschall@gmail.com>
2013-04-12 11:51:11 +0200

    register flusher

--- client/src/main/java/com/github/marschall/osgi/remoting/ejb/client/Activator.java
+++ client/src/main/java/com/github/marschall/osgi/remoting/ejb/client/Activator.java
@@ -1,17 +1,22 @@
 package com.github.marschall.osgi.remoting.ejb.client;
 
+import java.util.Hashtable;
+
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 
 import com.github.marschall.osgi.remoting.ejb.api.InitialContextService;
+import com.github.marschall.osgi.remoting.ejb.api.ProxyFlusher;
 
 public class Activator implements BundleActivator {
 
   private volatile ProxyService proxyService;
   private volatile LoggerBridge logger;
   private volatile ServiceReference<InitialContextService> serviceReference;
+  private volatile ServiceRegistration<ProxyFlusher> flusherRegisterService;
 
   @Override
   public void start(BundleContext context) throws Exception {
@@ -23,6 +28,8 @@ public class Activator implements BundleActivator {
 
     Bundle[] bundles = context.getBundles();
     this.proxyService.initialBundles(bundles);
+    
+    this.flusherRegisterService = context.registerService(ProxyFlusher.class, this.proxyService, new Hashtable<String, Object>());
   }
 
   @Override
@@ -31,10 +38,12 @@ public class Activator implements BundleActivator {
     this.proxyService.stop();
     this.logger.stop();
     context.ungetService(this.serviceReference);
+    this.flusherRegisterService.unregister();
 
     this.proxyService = null;
     this.logger = null;
     this.serviceReference = null;
+    this.flusherRegisterService = null;
   }
 
 }
