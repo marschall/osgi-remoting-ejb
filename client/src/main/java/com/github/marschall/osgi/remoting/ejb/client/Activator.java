a3da39d758d25e65e315351a04820f7b8bfe1d04
Philippe Marschall <philippe.marschall@gmail.com>
2013-04-12 12:28:32 +0200

    compiles again

--- client/src/main/java/com/github/marschall/osgi/remoting/ejb/client/Activator.java
+++ client/src/main/java/com/github/marschall/osgi/remoting/ejb/client/Activator.java
@@ -1,6 +1,9 @@
 package com.github.marschall.osgi.remoting.ejb.client;
 
 import java.util.Hashtable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
@@ -15,15 +18,19 @@ public class Activator implements BundleActivator {
 
   private volatile ProxyService proxyService;
   private volatile LoggerBridge logger;
-  private volatile ServiceReference<InitialContextService> serviceReference;
+  private volatile ServiceReference<InitialContextService> initialContextServiceReference;
   private volatile ServiceRegistration<ProxyFlusher> flusherRegisterService;
+  private volatile ScheduledExecutorService executor;
 
   @Override
   public void start(BundleContext context) throws Exception {
     this.logger = new LoggerBridge(context);
-    this.serviceReference = context.getServiceReference(InitialContextService.class);
+    this.initialContextServiceReference = context.getServiceReference(InitialContextService.class);
     
-    this.proxyService = new ProxyService(context, this.logger, context.getService(this.serviceReference));
+    this.executor = Executors.newSingleThreadScheduledExecutor(new LookUpThreadFactory());
+    
+    InitialContextService initialContextService = context.getService(this.initialContextServiceReference);
+    this.proxyService = new ProxyService(context, this.logger, initialContextService, this.executor);
     context.addBundleListener(this.proxyService);
 
     Bundle[] bundles = context.getBundles();
@@ -37,13 +44,24 @@ public class Activator implements BundleActivator {
     context.removeBundleListener(this.proxyService);
     this.proxyService.stop();
     this.logger.stop();
-    context.ungetService(this.serviceReference);
+    context.ungetService(this.initialContextServiceReference);
     this.flusherRegisterService.unregister();
+    this.executor.shutdownNow();
 
     this.proxyService = null;
     this.logger = null;
-    this.serviceReference = null;
+    this.initialContextServiceReference = null;
     this.flusherRegisterService = null;
+    this.executor = null;
+  }
+  
+  static final class LookUpThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable r) {
+      return new Thread(r, "osgi-remoting-ejb-proxy-lookup");
+    }
+    
   }
 
 }
